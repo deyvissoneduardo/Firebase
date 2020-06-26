5ef51e3ae0217146d26e3f0f8f253cf69a8c45cb
deyvisson eduardo <deyvissoneduardo22@gmail.com>
2020-06-26 14:21:46 -0300

    saving and changing in firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Salva no Banco de dados
-  Firestore.instance
-      .collection("usuarios")
-      .document("pontuacao")
-      .setData({"Deyvisson": "10", "Eduardo": "9"});
-
+  Firestore banco = Firestore.instance; // recupera a instancia do firebase
+  /*
+   *Salva no Banco de dados
+   * Para atualizar manten o document
+   *
+  banco.collection("usuarios")
+      .document("002")
+      .setData(
+      {
+        "nome": "Kelia Maiane",
+        "idade": "22"
+      });
+      */
+  // ---------------------------------------------------------------------//
+  /*
+   *Salva no Banco de dados
+   * com identificado geradd pelo firebase
+   */
+  DocumentReference ref =  await banco.collection("noticias")
+      .add(
+      {
+        "title": "Novo Planeta Descorberto",
+        "descricao": "texto do Campeão da America"
+      });
+  print("salvos: " + ref.documentID);
   runApp(AppFirebase());
 }
 
