3747b31fc03a9ea79b8e5a40857b552f6ab44a18
deyvisson eduardo <deyvissoneduardo22@gmail.com>
2020-06-26 23:46:15 -0300

    applying search filters

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Firestore banco = Firestore.instance; // recupera a instancia do firebase
   /*
-   *Salva no Banco de dados
-   * Para atualizar manten o document
-   *
+   // Salva no Banco de dados Para atualizar manten o document
   banco.collection("usuarios")
       .document("002")
       .setData(
@@ -15,11 +13,9 @@ void main() async {
         "nome": "Kelia Maiane",
         "idade": "22"
       });
-      */
   // ---------------------------------------------------------------------//
-  /*
-   *Salva no Banco de dados
-   * com identificado geradd pelo firebase
+
+   //Salva no Banco de dados com identificado geradd pelo firebase
   DocumentReference ref =  await banco.collection("noticias")
       .add(
       {
@@ -27,19 +23,12 @@ void main() async {
         "descricao": "texto do Campeão da America"
       });
   print("salvos: " + ref.documentID);
-  */
   // ---------------------------------------------------------------------//
-  /*
-   * Deleta usuario
-   */
-  // banco.collection("usuarios").document("003").delete();
+   // Deleta usuario
+   banco.collection("usuarios").document("003").delete();
   // ---------------------------------------------------------------------//
-  /*
-   * Recuperando um unico por id
-   */
-  /*
+   //Recuperando um unico por id
   DocumentSnapshot snapshot = await banco.collection("usuarios")
-  *
   .document("002")
   .get();
   var dados = snapshot.data;
@@ -52,17 +41,34 @@ void main() async {
   for ( DocumentSnapshot item in snapshot.documents ){
     var dados = item.data;
     print( "dados usuarios: " + dados.toString());
-  }*/
+  }
   // ---------------------------------------------------------------------//
-  /*
-  * Notifica sempre que houver alteração na colecao
-   */
+
+  // Notifica sempre que houver alteração na colecao
+
   banco.collection("usuarios").snapshots().listen((snapshots) {
     for (DocumentSnapshot item in snapshots.documents) {
       var dados = item.data;
       print("dados usuarios: " + dados.toString());
     }
   });
+   */
+  // Apliancando filtros
+  QuerySnapshot querySnapshot = await banco
+      .collection("usuarios")
+      // .where("nome", isEqualTo: "deyvisson")
+      //.where("idade", isEqualTo: 22)
+      .where("idade", isGreaterThan: 20) // maior que
+      //.where("idade", isLessThan: 35) // menor que
+      .orderBy("idade", descending: true)
+      .orderBy("nome", descending: false)
+      .limit(2)
+      .getDocuments();
+
+  for (DocumentSnapshot item in querySnapshot.documents) {
+    var dados = item.data;
+    print("dados usuarios: " + dados.toString());
+  }
   runApp(AppFirebase());
 }
 
